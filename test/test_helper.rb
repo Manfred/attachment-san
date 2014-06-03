07e56627a3627d1b681cec35d3ce6ac218ff5b9a
Manfred Stienstra <manfred@fngtps.com>
2014-06-03 17:30:28 +0200

    Switch to Peck in test helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,35 +1,8 @@
 TEST_ROOT_DIR = File.expand_path(File.dirname(__FILE__))
 
-frameworks = %w(activesupport activerecord actionpack)
-
-rails = [
-  File.expand_path('../../../rails', TEST_ROOT_DIR),
-  File.expand_path('../../rails', TEST_ROOT_DIR)
-].detect do |possible_rails|
-  begin
-    entries = Dir.entries(possible_rails)
-    frameworks.all? { |framework| entries.include?(framework) }
-  rescue Errno::ENOENT
-    false
-  end
-end
-
-raise "Couldn't find Rails directory!" unless rails
-
-frameworks.each { |framework| $:.unshift(File.join(rails, framework, 'lib')) }
-$:.unshift File.join(TEST_ROOT_DIR, '/../lib')
-$:.unshift TEST_ROOT_DIR
-
-ENV['RAILS_ENV'] = 'test'
-
-# Rails libs
-begin
-  require 'active_support'
-  require 'active_record'
-  require 'action_controller'
-rescue LoadError
-  raise "Please install Attachment-San as Rails plugin before running the tests."
-end
+require 'active_support'
+require 'active_record'
+require 'action_controller'
 
 require "pathname"
 module Rails
@@ -42,13 +15,10 @@ module Rails
   end
 end
 
+$:.unshift File.expand_path('../../lib', __FILE__)
 require File.expand_path('../../rails/init', __FILE__)
 
-# Libraries for testing
-require 'rubygems' rescue LoadError
 require 'sqlite3'
-require 'test/spec'
-require 'mocha'
 require 'fileutils'
 
 logdir = File.join(TEST_ROOT_DIR, 'log')
@@ -59,8 +29,63 @@ ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ":me
 TMP_DIR = File.join(TEST_ROOT_DIR, 'tmp')
 
 # Classes and methods to aid testing
-require 'lib/schema'
+Dir.glob(File.expand_path("../support/**/*.rb", __FILE__)).each { |file| require file }
+
+$:.unshift File.expand_path('../', __FILE__)
 
 require 'fixtures/models/attachment'
 require 'fixtures/models/document'
 require 'fixtures/models/options_stub'
+
+# Mocha support for Peck
+require 'mocha/api'
+
+class Peck
+  class Mocha
+    include ::Mocha::API
+
+    class AssertionCounter
+      def initialize(spec)
+        @spec = spec
+      end
+
+      def increment
+        @spec.expectations << 'Mocha'
+      end
+    end
+
+    def started_specification(spec)
+      mocha_setup
+    end
+
+    def finished_specification(spec)
+      mocha_verify(Peck::Mocha::AssertionCounter.new(spec))
+      mocha_teardown
+    end
+  end
+end
+
+# Libraries for testing
+require 'peck'
+require 'peck/delegates'
+
+# We need the Mocha delegate to receive events before the notifiers do
+Peck.delegates << Peck::Mocha.new
+
+require 'peck/counter'
+require 'peck/context'
+require 'peck/specification'
+require 'peck/expectations'
+require 'peck/notifiers/documentation'
+
+Peck::Notifiers::Documentation.use
+Peck.run_at_exit
+
+Peck::Context.once do |context|
+  include AttachmentSan::UploadHelpers
+
+  context.before do
+    FileUtils.rm_rf(TMP_DIR)
+    FileUtils.mkdir_p(TMP_DIR)
+  end
+end
