de8e9897e7925c005e321b818d1ad02f07dc111c
Manfred Stienstra <manfred@fngtps.com>
2014-06-03 17:49:23 +0200

    Set polymorphic source for all attachments.
    In the tests we need to set the polymorphic source for all attachments or Rails
    will try to find the foreign key.

--- test/fixtures/models/document.rb
+++ test/fixtures/models/document.rb
@@ -14,11 +14,11 @@ class MyProcessor
 end
 
 class Document < ActiveRecord::Base
-  has_attachment  :watermark
-  has_attachment  :logo, :variants => { :header => MyVariant }
+  has_attachment  :watermark, :as => :attachable
+  has_attachment  :logo, :as => :attachable, :variants => { :header => MyVariant }
   
-  has_attachments :misc_files, :class => MyOriginal, :filename_scheme => :keep_original, :process => proc { :from_process_proc }
-  has_attachments :images, :variants => {
+  has_attachments :misc_files, :as => :attachable, :class => MyOriginal, :filename_scheme => :keep_original, :process => proc { :from_process_proc }
+  has_attachments :images, :as => :attachable, :variants => {
     :thumbnail => proc { |v| MyProcessor.new(v) },
     :medium_sized => proc {},
     :download => proc {}
@@ -26,10 +26,10 @@ class Document < ActiveRecord::Base
 end
 
 class OtherDocument < ActiveRecord::Base
-  has_attachment  :logo, :variants => { :header => MyOtherVariant }
-  has_attachments :misc_files, :filename_scheme => :keep_original, :process => proc { :from_other_process_proc }
+  has_attachment  :logo, :as => :attachable, :variants => { :header => MyOtherVariant }
+  has_attachments :misc_files, :as => :attachable, :filename_scheme => :keep_original, :process => proc { :from_other_process_proc }
   
-  has_attachments :images, :base_path => '/other/base', :public_base_path => '/other/public', :variants => {
-    :normal => { :base_path => '/yet/another/base', :process => proc {} }
+  has_attachments :images, :as => :attachable, :base_path => File.join(TMP_DIR, '/other/base'), :public_base_path => '/other/public', :variants => {
+    :normal => { :base_path => File.join(TMP_DIR, 'yet/another/base'), :process => proc {} }
   }
 end
